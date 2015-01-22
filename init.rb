bedd40385ae66d4d353b5be85c9eb2a303dc212c
Mauricio Camayo <mauricio.camayo@gmail.com>
2015-01-22 12:13:53 -0500

    Changes in README

--- init.rb
+++ init.rb
@@ -12,13 +12,15 @@
 
 Redmine::Plugin.register :redmine_gamification_plugin do
   name 'Gamification plugin'
-  author 'hrtkzt'
+  author 'Mauricio Camayo'
   description 'This plugin is gamification plugin in redmine'
   version '0.0.1'
+  url 'https://github.com/mauricio-camayo/redmine_gamification_plugin'
+  author_url 'https://github.com/mauricio-camayo'
 
   permission :redmine_gamification_plugin, {:redmine_gamification_plugin => [:project]}, :public => true
 
-  menu :top_menu, :redmine_gamification_plugin, {controller: 'gamification', action: 'index'}, :caption => 'Gamification Application'
+  menu :top_menu, :redmine_gamification_plugin, {controller: 'gamification', action: 'index'}, :caption => 'Gamification'
   menu :project_menu, :project_gamification, {controller: 'gamification', action: 'project'}, caption: 'Status', param: :project_id 
 end
 
