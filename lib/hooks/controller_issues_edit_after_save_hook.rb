8e3190da91620dfef1e07df1aa54b9d73ba56ee2
Mauricio Camayo <mauricio.camayo@gmail.com>
2015-01-23 12:11:08 -0500

    Ability to select if using gamification on new/edit or just one or another

--- lib/hooks/controller_issues_edit_after_save_hook.rb
+++ lib/hooks/controller_issues_edit_after_save_hook.rb
@@ -6,51 +6,52 @@ module Hooks
     include GamificationUtil
 
     def controller_issues_edit_after_save(context={})
-      if context[:params] && context[:params][:issue]
-        if User.current.allowed_to?(:assign_deliverable_to_issue, context[:issue].project)
-          if context[:params][:issue][:deliverable_id].present?
-            deliverable = Deliverable.find_by_id(context[:params][:issue][:deliverable_id])
-            if deliverable.contract.project == context[:issue].project
-              context[:issue].deliverable = deliverable
+      # will use gamification for edit issues?
+      if Setting.plugin_redmine_gamification_plugin.has_key?("use_edit_issue")
+        if context[:params] && context[:params][:issue]
+          if User.current.allowed_to?(:assign_deliverable_to_issue, context[:issue].project)
+            if context[:params][:issue][:deliverable_id].present?
+              deliverable = Deliverable.find_by_id(context[:params][:issue][:deliverable_id])
+              if deliverable.contract.project == context[:issue].project
+                context[:issue].deliverable = deliverable
+              end
+
+            else
+              context[:issue].deliverable = nil
+            end
+          end
+          current_user_id = User.current.id
+          project_id = Issue.find(context[:params][:id]).project_id
+
+            # gamification_user_update
+            if Gamification.exists?({user_id: current_user_id})
+              user = Gamification.find_by_user_id(current_user_id)
+              user_badge = GamificationBadge.find_by_user_id(current_user_id)
+
+              user.up_point(Setting.plugin_redmine_gamification_plugin['edit_issue_score'].to_i)
+
+              # check level
+              old_lvl = user.level
+              new_lvl = decide_level(user.point)
+              user.level = check_level(old_lvl, new_lvl)
+
+              # update user badge
+              new_badge = check_badge(user_badge, user.level)
+              new_badge.save
+
+              user.up_ticket_count
+              user.save
             end
 
-          else
-            context[:issue].deliverable = nil
-          end
+            # gamification_project_update
+            if GamificationProject.exists?({user_id: current_user_id, project_id: project_id})
+              user_project = GamificationProject.find_by_user_id_and_project_id(current_user_id, project_id)
+              user_project.up_point(Setting.plugin_redmine_gamification_plugin['edit_issue_project_score'].to_i) 
+              user_project.up_ticket_count
+              user_project.save
+            end
         end
-        current_user_id = User.current.id
-        project_id = Issue.find(context[:params][:id]).project_id
-
-        # gamification_user_update
-        if Gamification.exists?({user_id: current_user_id})
-          user = Gamification.find_by_user_id(current_user_id)
-          user_badge = GamificationBadge.find_by_user_id(current_user_id)
-
-          user.up_point(Setting.plugin_redmine_gamification_plugin['edit_issue_score'].to_i)
-          
-          # check level
-          old_lvl = user.level
-          new_lvl = decide_level(user.point)
-          user.level = check_level(old_lvl, new_lvl)
-
-          # update user badge
-          new_badge = check_badge(user_badge, user.level)
-          new_badge.save
-
-          user.up_ticket_count
-          user.save
-        end
-
-        # gamification_project_update
-        if GamificationProject.exists?({user_id: current_user_id, project_id: project_id})
-          user_project = GamificationProject.find_by_user_id_and_project_id(current_user_id, project_id)
-          user_project.up_point(Setting.plugin_redmine_gamification_plugin['edit_issue_project_score'].to_i) 
-          user_project.up_ticket_count
-          user_project.save
-        end
-
       end
-
       return ''
     end
   end
--- lib/hooks/controller_wiki_edit_after_save_hook.rb
+++ lib/hooks/controller_wiki_edit_after_save_hook.rb
@@ -6,39 +6,39 @@ module Hooks
     include GamificationUtil
 
     def controller_wiki_edit_after_save(context={})
-      if context[:params]
-        current_user_id = User.current.id
-        project_id = Project.find_by_identifier(context[:params][:project_id]).id
+      # will use gamification for edit issues?
+      if Setting.plugin_redmine_gamification_plugin.has_key?("use_edit_wiki")
+        if context[:params]
+          current_user_id = User.current.id
+          project_id = Project.find_by_identifier(context[:params][:project_id]).id
 
+          # gamification_user_update
+          if Gamification.exists?({user_id: current_user_id})
+            user = Gamification.find_by_user_id(current_user_id)
+            user_badge = GamificationBadge.find_by_user_id(current_user_id)
 
-        # gamification_user_update
-        if Gamification.exists?({user_id: current_user_id})
-          user = Gamification.find_by_user_id(current_user_id)
-          user_badge = GamificationBadge.find_by_user_id(current_user_id)
+            user.up_point(Setting.plugin_redmine_gamification_plugin['edit_wiki_score'].to_i)
 
-          user.up_point(Setting.plugin_redmine_gamification_plugin['edit_wiki_score'].to_i)
+            # check level
+            old_lvl = user.level
+            new_lvl = decide_level(user.point)
+            user.level = check_level(old_lvl, new_lvl)
 
-          # check level
-          old_lvl = user.level
-          new_lvl = decide_level(user.point)
-          user.level = check_level(old_lvl, new_lvl)
+            # update user badge
+            new_badge = check_badge(user_badge, user.level)
+            new_badge.save
 
-          # update user badge
-          new_badge = check_badge(user_badge, user.level)
-          new_badge.save
+            user.save
+          end
 
-          user.save
+          # gamification_project_update
+          if GamificationProject.exists?({user_id: current_user_id, project_id: project_id})
+            user_project = GamificationProject.find_by_user_id_and_project_id(current_user_id, project_id)
+            user_project.up_point(Setting.plugin_redmine_gamification_plugin['edit_wiki_project_score'].to_i)
+            user_project.save
+          end
         end
-
-        # gamification_project_update
-        if GamificationProject.exists?({user_id: current_user_id, project_id: project_id})
-          user_project = GamificationProject.find_by_user_id_and_project_id(current_user_id, project_id)
-          user_project.up_point(Setting.plugin_redmine_gamification_plugin['edit_wiki_project_score'].to_i)
-          user_project.save
-        end
-
       end
-
       return ''
     end
   end
